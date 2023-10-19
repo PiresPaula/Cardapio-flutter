1cc1f457d3a044fd2bf3542db433be0cd9793286
Paula Pires <104331877+PiresPaula@users.noreply.github.com>
2023-10-19 10:23:24 -0300

    Cardapio_atualizado aula 19/10

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cardapio/telas/tela_produtos.dart';
 import 'package:flutter/material.dart';
 import 'package:cardapio/models/produtos.dart';
 import 'utils/rotas.dart';
+
 void main() {
   runApp(AppCardapio());
 }
@@ -13,25 +14,17 @@ class AppCardapio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Cardápio",
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        fontFamily: 'Schyler',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          titleSmall: const TextStyle(
-            fontSize: 20,
-            fontFamily: 'Schyler'
-          )
-        )
-      ),
-      //home: TelaCategorias(),
-       routes: {
-        Rotas.HOME: (context) => TelaCategorias(),
-        Rotas.PRODUTOS: (context) => TelaProdutos(produtosValidos),
-      }
-    );
+        title: "Cardápio",
+        theme: ThemeData(
+            primarySwatch: Colors.blueGrey,
+            fontFamily: 'Schyler',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                titleSmall:
+                    const TextStyle(fontSize: 20, fontFamily: 'Schyler'))),
+        //home: TelaCategorias(),
+        routes: {
+          Rotas.HOME: (context) => TelaCategorias(),
+          Rotas.PRODUTOS: (context) => TelaProdutos(produtosValidos),
+        });
   }
 }
-
-
-
